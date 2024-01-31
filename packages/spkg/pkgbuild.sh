270893939253b017dc26cff26e64f7f651b6b0fc
honakac <honakac@yandex.ru>
2024-01-31 18:21:00 +0500

    fixed spkg/pkgbuild.sh

--- packages/spkg/pkgbuild.sh
+++ packages/spkg/pkgbuild.sh
@@ -3,7 +3,6 @@ pkg_version="0.1"
 pkg_depends="bash coreutils gawk wget tar xz"
 
 pkg_src="https://github.com/sourimlinux/spkg/releases/download/v$pkg_version/spkg-$pkg_version.tar.xz"
-pkg_src_dir="spkg-$pkg_version"
 
 pkg_build() {
     make DESTDIR=$1 install
